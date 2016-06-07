2154fb6407a9912ae84947f220ab63076d2d57b6
razvantomegea <razvan.tomegea@gmail.com>
2016-06-07 18:16:00 +0300

    add: sidenavigation and new pages

--- app/app.ts
+++ app/app.ts
@@ -1,16 +1,20 @@
-import { Component } from "@angular/core";
-import { Platform, ionicBootstrap } from 'ionic-angular';
+import { Component, ViewChild } from "@angular/core";
+import { Platform, ionicBootstrap, Nav } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
 import { 
-  FIREBASE_PROVIDERS,
-  defaultFirebase,
   AngularFire,
-  firebaseAuthConfig,
+  AuthMethods,
   AuthProviders,
-  AuthMethods
+  defaultFirebase,
+  firebaseAuthConfig,
+  FIREBASE_PROVIDERS
 } from 'angularfire2';
 
+import { FitnessPage } from './pages/fitness/fitness'
 import { HomePage } from './pages/home/home';
+import { NutrientsPage } from './pages/nutrients/nutrients';
+import { RecipesPage } from './pages/recipes/recipes';
+
 
 
 @Component({
@@ -21,10 +25,24 @@ import { HomePage } from './pages/home/home';
   ]
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage: any = HomePage;
+  pages: Array<{title: string, component: any}>
 
   constructor(platform: Platform) {
     platform.ready().then(() => StatusBar.styleDefault());
+    this.pages = [
+      { title: 'Fitness', component: FitnessPage },
+      { title: 'Home', component: HomePage },
+      { title: 'Nutrients', component: NutrientsPage },
+      { title: 'Recipes', component: RecipesPage }
+    ];
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 }
 
