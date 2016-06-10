7e86f86cdf72b3c4cccae291a9b76f38f09bc092
razvantomegea <razvan.tomegea@gmail.com>
2016-06-10 15:31:42 +0300

    add: full nutrient intake style and details

--- app/pages/nutrients/nutrient-details/nutrient-details.ts
+++ app/pages/nutrients/nutrient-details/nutrient-details.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { NavParams } from 'ionic-angular';
+import { MATERIAL_DIRECTIVES } from 'ng2-material';
 
 import { INutrient, NutrientService } from '../shared';
 
 @Component({
-    templateUrl: 'build/pages/nutrients/nutrient-details/nutrient-details.html'
+    templateUrl: 'build/pages/nutrients/nutrient-details/nutrient-details.html',
+    directives: [MATERIAL_DIRECTIVES]
 })
 export class NutrientDetailsPage implements OnInit {
     nutrient: INutrient;
+    intakeUnit: string;
     constructor(params: NavParams) { 
         this.nutrient = params.data.nutrient;
+        this.intakeUnit = (this.nutrient.category === 'Vitamin' || this.nutrient.category === 'Mineral') ? 'mg' : 'g';
     }
 
-    ngOnInit() { }
+    ngOnInit() {
+
+    }
 
 }
