e8265ebe452b9c56c1b28d5be5d2ad4067d09650
razvantomegea <razvan.tomegea@gmail.com>
2016-06-30 19:22:19 +0300

    add: new conditions and credit sources

--- app/pages/credits/credits.ts
+++ app/pages/credits/credits.ts
@@ -14,8 +14,20 @@ export class CreditsPage implements OnInit{
   ngOnInit(): void {
     this.creditsList = [
       {
-        name: "Earth Clinic",
+        name: "Earthclinic",
         url: "http://www.earthclinic.com/"
+      },
+      {
+        name: "B4Tea",
+        url: "http://b4tea.com/food-health/natural-remedies-for-kidney-failure-problems/"
+      },
+      {
+        name: "Healthline",
+        url: "http://www.healthline.com/health/skin-disorders"
+      },
+      {
+        name: "Mayoclinic",
+        url: "http://www.healthline.com/"
       }
     ];
   }
